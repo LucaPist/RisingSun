d749d7a446dab510c5cbc8356b08bbb45da7cfa0
LucaPist <105346197+LucaPist@users.noreply.github.com>
2022-06-22 10:21:17 +0200

    Aggiungere i file di progetto.

---
+++ WebAPI-Sample2/Helper/phonenumber.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebAPI_Sample2.Helper
+{
+     public static class PhoneNumber
+        {
+            // Regular expression used to validate a phone number.
+            public const string motif = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+
+            public static bool IsPhoneNbr(this string number)
+            {
+                if (number != null) return Regex.IsMatch(number, motif);
+                else return false;
+            }
+        
+    }
+
+
+}
---
+++ WebAPI-Sample2/Models/UserInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_Sample2.Models
+{
+    public class UserInfo
+    {
+        public Guid UserId { get; set; } = Guid.NewGuid();
+        [Required]
+        public string? DisplayName { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+        [EmailAddress]
+        public string? Email2 { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public string? Cognome { get; set; }
+        [Required]
+        public DateTime? DataNasc { get; set; }
+        [Required]
+        public string? Indirizzo { get; set; }
+        [Required]
+        public string? CAP { get; set; }
+        [Required]
+        public string? Telfono1 { get; set; }
+        
+        public string? Telfono2 { get; set; }
+        [Required]
+
+        public Boolean? Conf { get; set; }    //conf privacy//
+
+        
+        public DateTime? CreatedDate { get; set; }
+    }
+}
